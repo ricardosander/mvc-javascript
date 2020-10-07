d4159a5ca26e5542ca2a9ccd6af3a6763cd4944b
Ricardo Sander Lopes <ricardo.lopes@ifood.com>
2020-10-07 12:35:58 -0300

    Criando modelo para lista de negociações.

---
+++ client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,15 @@
+class ListaNegociacoes {
+
+    constructor() {
+        this._negociacoes = [];
+    }
+
+    adicionar(negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+        return [].concat(this._negociacoes);
+    }
+
+}
