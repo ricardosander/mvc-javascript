9a56964bdcdc9585c97e2e6db059acac52ded9c1
Ricardo Sander Lopes <ricardo.lopes@ifood.com>
2020-11-08 14:45:38 -0300

    Simplificando tratamento duplo de promises.

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -5,66 +5,53 @@ class NegociacaoService {
     }
 
     adicionar(negociacao) {
-
-        return new Promise((resolve, reject) => {
-
-            this._httpService.post('/negociacoes', negociacao)
-                .then(negociacao => resolve(negociacao))
-                .catch(erro => {
-                    console.log(`Houve um erro ao enviar uma nova negociação para cadastro: ${erro}`);
-                    reject('Não foi possível salvar a nova negociação.');
-                });
-        });
+        return this._httpService.post('/negociacoes', negociacao)
+            .then(negociacao => negociacao)
+            .catch(erro => {
+                console.log(`Houve um erro ao enviar uma nova negociação para cadastro: ${erro}`);
+                throw new Error('Não foi possível salvar a nova negociação.');
+            });
     }
 
     importarNegociacoesDaSemana() {
 
-        return new Promise((resolve, reject) => {
-            
-            console.log("Importando negociações...");
+        console.log("Importando negociações...");
 
-            this._httpService.get('/negociacoes/semana')
-                .then(negociacoes => {
-                    resolve(negociacoes.map(negociacao => new Negociacao(new Date(negociacao.data), negociacao.quantidade, negociacao.valor)));
-                })
-                .catch(erro => {
-                    console.log(`Houve um erro ao carregar as negociações: ${erro}`);
-                    reject('Não foi possível importar as negociações.');
-                });
-        });
+        return this._httpService.get('/negociacoes/semana')
+            .then(negociacoes => {
+                return negociacoes.map(negociacao => new Negociacao(new Date(negociacao.data), negociacao.quantidade, negociacao.valor));
+            })
+            .catch(erro => {
+                console.log(`Houve um erro ao carregar as negociações: ${erro}`);
+                throw new Error('Não foi possível importar as negociações.');
+            });
     }
 
     importarNegociacoesDaSemanaAnterior() {
 
-        return new Promise((resolve, reject) => {
-            
-            console.log("Importando negociações da semana anterior...");
+        console.log("Importando negociações da semana anterior...");
 
-            this._httpService.get('/negociacoes/anterior')
-                .then(negociacoes => {
-                    resolve(negociacoes.map(negociacao => new Negociacao(new Date(negociacao.data), negociacao.quantidade, negociacao.valor)));
-                })
-                .catch(erro => {
-                    console.log(`Houve um erro ao carregar as negociações da semana anterior: ${erro}`);
-                    reject('Não foi possível importar as negociações da semana anterior.');
-                });
-        });
+        return this._httpService.get('/negociacoes/anterior')
+            .then(negociacoes => {
+                return negociacoes.map(negociacao => new Negociacao(new Date(negociacao.data), negociacao.quantidade, negociacao.valor));
+            })
+            .catch(erro => {
+                console.log(`Houve um erro ao carregar as negociações da semana anterior: ${erro}`);
+                throw new Error('Não foi possível importar as negociações da semana anterior.');
+            });
     }
 
     importarNegociacoesDaSemanaRetrasada() {
 
-        return new Promise((resolve, reject) => {
-            
-            console.log("Importando negociações da semana retrasada...");
+        console.log("Importando negociações da semana retrasada...");
 
-            this._httpService.get('/negociacoes/retrasada')
-                .then(negociacoes => {
-                    resolve(negociacoes.map(negociacao => new Negociacao(new Date(negociacao.data), negociacao.quantidade, negociacao.valor)));
-                })
-                .catch(erro => {
-                    console.log(`Houve um erro ao carregar as negociações da semana retrasada: ${erro}`);
-                    reject('Não foi possível importar as negociações retrasada.');
-                });
-        });
+        return this._httpService.get('/negociacoes/retrasada')
+            .then(negociacoes => {
+                return negociacoes.map(negociacao => new Negociacao(new Date(negociacao.data), negociacao.quantidade, negociacao.valor));
+            })
+            .catch(erro => {
+                console.log(`Houve um erro ao carregar as negociações da semana retrasada: ${erro}`);
+                throw new Error('Não foi possível importar as negociações retrasada.');
+            });
     }
 }
